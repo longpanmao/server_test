f56b13c7b8c807cc3cafa0dee19f7e3dcf32f2e6
maomao <maomao@abc.com>
2017-07-26 15:11:20 +0700

    接口

---
+++ php/Properties.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * @Description 668手游产品通用接口范例 
+ * @V3.0
+ * @Author rui.xin
+ */
+
+#	商户编号p1_MerId,以及密钥merchantKey 需要从普讯科技101卡平台获得
+$p1_MerId			= "8880004";																										#测试使用
+$merchantKey	= "6dcc3a7a2a854569846062c206d2ff3e";		#测试使用
+
+$logName	= "BANK_HTML.log";
+
+?> 
